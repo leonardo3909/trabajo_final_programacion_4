d6c7953c43b9a6e31cda0cddb570673ae326ff44
JohnJaider955 <jaidercardona955@gmail.com>
2024-05-17 17:42:32 -0500

    Actualización navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,43 @@ import './App.css'
 function App() {
   return (
     <>
-    <div className="container">
-      <h1>Top Scorers</h1>
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Buscar por nombre jugador..."
-          value={searchTerm}
-          onChange={handleSearch}
-        />
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
       </div>
-      <button onClick={handleSort}>
-        Ordenar por goles ({sortOrder === 'asc' ? 'menor a mayor' : 'mayor a menor'})
-      </button>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Player</th>
-            <th>Position</th>
-            <th>Goals</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortedPlayers.map((player, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{player.name}</td>
-              <td>{player.position}</td>
-              <td>{player.goals}</td>
-              <td>{player.age}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    </nav>
+    
     </>
   )
 }
